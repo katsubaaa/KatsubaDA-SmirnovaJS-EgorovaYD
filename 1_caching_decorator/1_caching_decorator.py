fcd470960093b9c2ea153f88499710601a9a752d
YanaED <132608913+YanaED@users.noreply.github.com>
2024-11-01 16:17:25 +0300

    Update 1_caching_decorator.py

--- 1_caching_decorator/1_caching_decorator.py
+++ 1_caching_decorator/1_caching_decorator.py
@@ -1,17 +1,19 @@
-import functools
+  import functools
 
-def caching_decorator(max_cache_size=128):
-  cache = {}
-  def decorator(func):
-    @functools.wraps(func)
-    def wrapper(*args):
-      if args in cache:
-        return cache[args]
-      result = func(*args)
-      if len(cache) >= max_cache_size:
-        cache.pop(next(iter(cache)))  # Удаляем старейший элемент
-      cache[args] = result
-      return result
-    return wrapper
-  return decorator
+  def caching_decorator(max_cache_size=128):
+    cache = {}
+    
+    def decorator(func):
+      @functools.wraps(func)
+      def wrapper(*args):
+        if args in cache:
+          return cache[args]
+        result = func(*args)
+        if len(cache) >= max_cache_size:
+          cache.pop(next(iter(cache)))  # Удаляем старейший элемент
+        cache[args] = result
+        return result
+      return wrapper
+      
+    return decorator
 
