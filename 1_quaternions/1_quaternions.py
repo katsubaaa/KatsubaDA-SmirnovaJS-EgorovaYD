6cfa4fe47bbc6a109120a05edde5eb8471ebd7bc
YanaED <132608913+YanaED@users.noreply.github.com>
2024-11-01 15:47:22 +0300

    Update 1_quaternions.py

--- 1_quaternions/1_quaternions.py
+++ 1_quaternions/1_quaternions.py
@@ -28,3 +28,22 @@ class Quaternion:
         y = self.w * other.y - self.x * other.z + self.y * other.w + self.z * other.x
         z = self.w * other.z + self.x * other.y - self.y * other.x + self.z * other.w
         return Quaternion(w, x, y, z)
+
+    def scale(self, scalar):
+        """Вычисляет произведение кватерниона на скалярную величину."""
+        return Quaternion(self.w * scalar, self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def norm(self):
+        """Вычисляет норму кватерниона."""
+        return (self.w**2 + self.x**2 + self.y**2 + self.z**2) ** 0.5
+
+    def normalize(self):
+        """Нормализует кватернион (приводит к единичной норме)."""
+        norm_value = self.norm()
+        if norm_value == 0:
+            raise ValueError("Cannot normalize zero quaternion")
+        return self.scale(1 / norm_value)
+
+    def dot(self, other):
+        """Вычисляет скалярное произведение двух кватернионов."""
+        return self.w * other.w + self.x * other.x + self.y * other.y + self.z * other.z
