00dbd044869df8ad7066eaa2732230d78298cc43
katsubaaa <129784484+katsubaaa@users.noreply.github.com>
2024-11-27 14:23:38 +0300

    Update 1_caching_decorator.py

--- 1_caching_decorator/1_caching_decorator.py
+++ 1_caching_decorator/1_caching_decorator.py
@@ -26,4 +26,25 @@
   def slow_function2(x):
       print(f"Вычисляем {x}...")
       return x + 2
-    
+
+# Первый вызов - вычисление
+print(slow_function(2))  # Вычисляем 2...
+print(slow_function(3))  # Вычисляем 3...
+print(slow_function(4))  # Вычисляем 4...
+
+# Повторные вызовы - из кэша
+print(slow_function(2))  # Из кэша
+print(slow_function(3))  # Из кэша
+
+# Добавим еще один элемент, чтобы проверить ограничение кэша
+print(slow_function(5))  # Вычисляем 5...
+
+# Проверяем кэш: 2 и 3 должны быть в кэше, а 4 должен быть удален
+print(slow_function(2))  # Вычисляем заново, так как он был удален из кэша
+
+print(slow_function2(1)) # Вычисляем 1...
+print(slow_function2(2)) # Вычисляем 2...
+print(slow_function2(3)) # Вычисляем 3...
+
+print(slow_function(4)) # Из кэша
+print(slow_function2(1)) # Из кэша
