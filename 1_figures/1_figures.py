a9a708c482457562ce9d5f621970ab473c7234f7
iuliasmirno <iulia.smirno@gmail.com>
2024-12-03 19:05:38 +0300

    Update 1_figures.py

--- 1_figures/1_figures.py
+++ 1_figures/1_figures.py
@@ -1,1 +1,33 @@
+class Shape:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
+class Rectangle(Shape): 
+    def __init__(self, width, height, x=0, y=0): 
+        super().__init__(x, y)
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    def area(self):
+        return self.width * self.height
+
+class Square(Rectangle):
+    def __init__(self, side, x=0, y=0):
+        super().__init__(side, side, x, y)
